fb409b027a6c8b8b08fc99fd2a8b8fedee3b9296
Fred Huang <fred.huang@nexusguard.com>
2016-03-30 09:43:26 +0800

    re-org to python dist package

---
+++ src/YamlLibrary/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from fetchyaml import FetchYaml
+
+__version__ = '0.1'
+
+class YamlLibrary(FetchYaml):
+    """
+    TO BE DEFINED
+    """
+
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
